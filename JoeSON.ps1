499877c8d7aa581b51c8eeec632c1635cfe23cdc
Joe Crapo <jcrapo@epic.com>
2018-06-12 12:25:30 -0500

    first push

---
+++ JoeSON.ps1
@@ -0,0 +1,12 @@
+﻿$url = 'http://letsrevolutionizetesting.com/challenge.json'
+
+$response = Invoke-WebRequest -Uri $url
+
+while ($response.Content -like "*follow*")
+{
+    $urlid = (ConvertFrom-Json -InputObject $response.Content).follow.Split("=")[1]
+    $url = "http://letsrevolutionizetesting.com/challenge.json?id=" + $urlid
+    $response = Invoke-WebRequest -Uri $url
+}
+
+$response.Content
